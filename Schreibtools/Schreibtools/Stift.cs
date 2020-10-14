1ea65206e6c7092a2dad7c84ce05935ad8cf3a18
MatthiasEisenhut <eisenhut.m03@htlwienwest.at>
2020-10-14 11:59:13 +0200

    Stand 14.10.2020, 11:58

---
+++ Schreibtools/Schreibtools/Stift.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schreibtools
+{
+    abstract class Stift
+    {
+        public ConsoleColor Farbe { get; set; }
+        public string Typ { get; set; }
+        public double Strichstärke { get; set; }
+
+        public Stift(double Strichstärke)
+        {
+            this.Farbe = 0;
+            this.Typ = "unbekannt";
+            this.Strichstärke = Strichstärke;
+        }
+        public Stift(double Strichstärke, ConsoleColor Farbe)
+        {
+            this.Strichstärke = Strichstärke;
+        }
+        public string SchreibeText(string text)
+        {
+
+        }
+    }
+}
